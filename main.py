77c7f8f6e062f8746793b6486759b23ff08c60e1
solar-shy <72438486+solar-shy@users.noreply.github.com>
2020-11-17 19:10:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,78 @@
 import pygame
+import random
+import sys
+import os
+import time
 
-
-W = 800
-H = 600
-a = (255, 255, 255)
-b = (255, 255, 0)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-rect_1_color = (255, 0, 255)
-rect_1_rect = ((10, 10), (100, 100))
-rect_1_width = 4
+os.environ['SDL_VIDEO_WINDOW_POS'] = '0,30'
 
 pygame.init()
-screen = pygame.display.set_mode((W, H))
-pygame.display.set_caption('Текст')
-screen.fill(BLUE)
-pygame.display.flip()
-pygame.mouse.set_visible(False)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+Info = pygame.display.Info()
+W, H = Info.current_w, Info.current_h
+
+MAX_SNOW = 250
+BG_COLOR = (25, 25, 25)
 
 
-font = pygame.font.SysFont('Arial', 96, True, False)
-font2 = pygame.font.SysFont('Arial', 48, True, False)
-font_box = pygame.Surface((W - 30, font.get_height()))
-font2_box = pygame.Surface((W - 30, font2.get_height()))
+class Snow(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
 
-text = font.render("Всем привет", True, a)
-screen.blit(text, (100, 200))
-text = font2.render("задание на урок", True, b)
-screen.blit(text, (215, 350))
-pygame.draw.rect(screen, rect_1_color, rect_1_width)
+        self.speed = random.randint(1, 3)
+        self.img_num = random.randint(1, 2)
+        self.size = random.randint(10, 64)
+        self.image_filename = f'snowflake{self.img_num}.png'
+        self.image_orig = pygame.image.load(self.image_filename)
+        self.image_orig = pygame.transform.scale(self.image_orig,
+                                                 (self.size, self.size))
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect(center=(x, y))
+
+        self.rot = 0
+        self.angle = random.randrange(-1, 2, 2)
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.top > H:
+            self.rect.bottom = 0
+
+        self.rot = (self.rot + self.angle) % 360
+        self.image = pygame.transform.rotate(self.image_orig, self.rot)
+        self.rect = self.image.get_rect(center=self.rect.center)
 
 
-run = True
-while run:
+def check_for_exit():
     for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            run = False
-        elif e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_ESCAPE:
-                run = False
+        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN \
+                and e.key == pygame.K_ESCAPE:
+            sys.exit(0)
 
+
+'______________________________ MAIN _____________________________'
+
+pygame.display.set_icon(pygame.image.load('snow.ico'))
+pygame.display.set_caption('Jiminshee')
+screen = pygame.display.set_mode((W, H))
+
+
+def init_snow(max_snow):
+    for _ in range(max_snow):
+        snowgroup.add(Snow(random.randint(0, W), random.randint(0, H)))
+
+
+snowgroup = pygame.sprite.Group()
+init_snow(MAX_SNOW)
+
+
+while True:
+    check_for_exit()
+    snowgroup.update()
+    screen.fill(BG_COLOR)
+    snowgroup.draw(screen)
     pygame.display.update()
+    clock.tick(FPS)
+
